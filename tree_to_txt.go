4ed95cc5a49d2307eb26e84031fb146b2ec08ee5
Jamesbarford <jamesbarfordevans@gmail.com>
2020-09-20 10:55:29 +0100

    ability to create a txt representation of the *item tree

---
+++ tree_to_txt.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+const (
+	INDENTATION = "\t"
+	NEW_LINE    = "\n"
+	PREFIX      = "- "
+)
+
+type ItemIteratee = func(i *item, depth int)
+
+func treeToTxt(tree *item, fileName string) {
+	err := ioutil.WriteFile(fileName+".txt", *treeToBytes(tree), 0644)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func treeToBytes(tree *item) *[]byte {
+	var bytes []byte
+
+	// purposely ignore root node, as it is readonly
+	treeIterator(tree, 0, func(i *item, depth int) {
+		bytes = append(bytes, []byte(formattedString(i.Head, depth))...)
+	})
+
+	return &bytes
+}
+
+func treeIterator(i *item, depth int, iteratee ItemIteratee) {
+	i.ForEachChild(func(t *item, _ int) {
+		iteratee(t, depth)
+		if !t.IsLeaf() {
+			treeIterator(t, depth+1, iteratee)
+		}
+	})
+}
+
+func formattedString(head string, depth int) string {
+	return addIndentation(depth) + PREFIX + head + NEW_LINE
+}
+
+func addIndentation(depth int) string {
+	indents := ""
+
+	if depth == 0 {
+		return indents
+	}
+
+	for i := depth; i > 0; i-- {
+		indents += INDENTATION
+	}
+
+	return indents
+}
