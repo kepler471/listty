1cdd76818c5e9354f63c00386e25929bfd22229c
Stelios Georgiou <steliosdg@gmail.com>
2020-09-24 21:51:43 +0100

    new implementation, split the list, will work with a change to item.Tail signature

--- parser_test.go
+++ parser_test.go
@@ -5,17 +5,27 @@ import "testing"
 //func TestParseTxt(t *testing.T) {
 //	parseTxt()
 //}
+//
+//func TestParseTxt2Flat(t *testing.T) {
+//	parseTxt2("flat")
+//}
+//func TestParseTxt2Indents(t *testing.T) {
+//	parseTxt2("indents")
+//}
+//func TestParseTxt2VariousShort(t *testing.T) {
+//	parseTxt2("all_short")
+//}
+//
+//func TestParseTxt2VariousLong(t *testing.T) {
+//	parseTxt2("example2")
+//}
 
-func TestParseTxt2Flat(t *testing.T) {
-	parseTxt2("flat")
+func TestParseTxt3Short(t *testing.T) {
+	parseTxt3("textfiles/all_short")
 }
-func TestParseTxt2Indents(t *testing.T) {
-	parseTxt2("indents")
+func TestParseTxt3Flat(t *testing.T) {
+	parseTxt3("textfiles/flat")
 }
-func TestParseTxt2VariousShort(t *testing.T) {
-	parseTxt2("all_short")
-}
-
-func TestParseTxt2VariousLong(t *testing.T) {
-	parseTxt2("example2")
+func TestParseTxt3Long(t *testing.T) {
+	parseTxt3("textfiles/example2")
 }
