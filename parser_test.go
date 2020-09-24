77263941a20bb6a6a099865e1d2d7c1b8aa1d014
Stelios Georgiou <steliosdg@gmail.com>
2020-09-24 22:10:03 +0100

    some cleanup

--- parser_test.go
+++ parser_test.go
@@ -2,30 +2,12 @@ package main
 
 import "testing"
 
-//func TestParseTxt(t *testing.T) {
-//	parseTxt()
-//}
-//
-//func TestParseTxt2Flat(t *testing.T) {
-//	parseTxt2("flat")
-//}
-//func TestParseTxt2Indents(t *testing.T) {
-//	parseTxt2("indents")
-//}
-//func TestParseTxt2VariousShort(t *testing.T) {
-//	parseTxt2("all_short")
-//}
-//
-//func TestParseTxt2VariousLong(t *testing.T) {
-//	parseTxt2("example2")
-//}
-
 func TestParseTxt3Short(t *testing.T) {
-	parseTxt3("textfiles/all_short")
+	parseTxt("textfiles/all_short")
 }
 func TestParseTxt3Flat(t *testing.T) {
-	parseTxt3("textfiles/flat")
+	parseTxt("textfiles/flat")
 }
 func TestParseTxt3Long(t *testing.T) {
-	parseTxt3("textfiles/example2")
+	parseTxt("textfiles/example2")
 }
