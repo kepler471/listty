4b429887b043f7bba071fc474082ac2558256041
Stelios Georgiou <steliosdg@gmail.com>
2020-09-25 17:30:15 +0100

    clear whitespace from lines, removed usage of depth, added file extension check

--- parser.go
+++ parser.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"path"
 	"strings"
 )
 
@@ -11,24 +12,29 @@ const (
 	indent   = "\t"
 )
 
-func parseTxt(filename string) {
-	f, err := ioutil.ReadFile(filename + ".txt")
+// parseTxt creates a item tree structure from a txt, and returns the root item
+func parseTxt(filename string) *item {
+	if !strings.HasSuffix(filename, ".txt") {
+		filename += ".txt"
+	}
+	// TODO: use a Reader, so the whole file is not loaded at once
+	f, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal("Did not load file: ", err)
 	}
 	s := string(f)
-	root := item{
-		Home:   true,
-		Parent: nil,
-		Head:   "root",
-		Tail:   nil,
-		depth:  -1,
-	}
 	lines := strings.Split(s, nextItem)
-
+	root := item{
+		Home: true,
+		Head: strings.TrimSuffix(path.Dir(filename), ".txt"),
+	}
 	splitAndSearch(&root, -1, lines)
+	return &root
 }
 
+// splitAndSearch creates a tree structure on a given item i, by searching for
+// indentations in lines of text. It will recursively search on all direct children
+// of i, with subgroups of the original set of lines.
 func splitAndSearch(i *item, depth int, lines []string) {
 	depth++
 	var splits []int
@@ -37,24 +43,19 @@ func splitAndSearch(i *item, depth int, lines []string) {
 		if d == depth {
 			splits = append(splits, n)
 			line := item{
-				Home:   false,
 				Parent: i,
-				Head:   l,
-				Tail:   nil,
-				depth:  depth,
+				Head:   strings.TrimLeft(l, "\t"),
 			}
 			i.Tail = append(i.Tail, &line)
 		}
 	}
+	// splits recorded the indices at which to divide lines, and the function
+	// is then called on each of these subgroups, to search the next depth level.
 	for n, t := range i.Tail {
 		if splits[n] == splits[len(splits)-1] {
-			group := lines[splits[n]:]
-			splitAndSearch(t, depth, group)
+			splitAndSearch(t, depth, lines[splits[n]:])
 		} else {
-			group := lines[splits[n]:splits[n+1]]
-			splitAndSearch(t, depth, group)
+			splitAndSearch(t, depth, lines[splits[n]:splits[n+1]])
 		}
 	}
 }
-
-// TODO: Add func to clean escape characters from line
