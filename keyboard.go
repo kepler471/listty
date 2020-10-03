54e2c1b688cdf03fbb11c6603dce63d5202a7cb8
Stelios Georgiou <steliosdg@outlook.com>
2020-10-03 19:18:45 +0100

    Introduced editing mode, splitting key events

--- keyboard.go
+++ keyboard.go
@@ -5,21 +5,77 @@ import (
 	"os"
 )
 
-func handleEventKey(ev *tcell.EventKey, s tcell.Screen, c *Cursor) {
+// EditMode is a flag used to restrict keyboard actions. In edit mode, the user
+//	can write text to an item. Outside of edit mode, the user can perform all
+// 	the tree manipulation actions.
+type EditMode bool
+
+func handleEventKey(ev *tcell.EventKey, s tcell.Screen, c *Cursor, m *EditMode) {
+	if *m {
+		handleEdit(ev, s, c, nil)
+	} else {
+		handleManipulate(ev, s, c, nil)
+	}
+}
+
+// handleEdit controls the keyboard actions in EditMode
+func handleEdit(ev *tcell.EventKey, s tcell.Screen, c *Cursor, m *EditMode) {
 	switch ev.Key() {
-	case tcell.KeyEscape:
-		s.Fini()
-		os.Exit(0)
 	case tcell.KeyEnter:
-		newItem(c.i)
+		// S-Enter creates a new item below, and saves any changes
+		if ev.Modifiers() == 1 {
+			// TODO: newItem needs fixing
+			// It needs to be aware of the current item's tail, and should
+			// 	add if there is a tail, that is expanded, it should add to
+			// 	the top of that tail.
+			// It seems like the pointer to the new item should be returned,
+			// 	which could be a way to ensure cursor is directed correctly.
+			newItem(c.i)
+			c.Down()
+			return // to maintain editing state
+		}
+		// C-S-Enter could create a new item above, and saves and changes
+		// 	if in edit mode, however it seems difficult to access this keybind
+
+		// Leave edit mode by saving changes to item head
+		*m = false
 	case tcell.KeyBackspace2:
 	case tcell.KeyUp:
 		c.Up()
 	case tcell.KeyDown:
 		c.Down()
-	case tcell.KeyRune:
-	case tcell.KeyRight:
 	case tcell.KeyLeft:
+	case tcell.KeyRight:
+	case tcell.KeyRune:
+	case tcell.KeyTab:
+	case tcell.KeyBacktab:
+	}
+}
+
+// handleManipulate controls the keyboard actions when not in EditMode
+func handleManipulate(ev *tcell.EventKey, s tcell.Screen, c *Cursor, m *EditMode) {
+	switch ev.Key() {
+	case tcell.KeyEscape:
+		//
+		s.Fini()
+		os.Exit(0)
+	case tcell.KeyEnter:
+		// S-Enter creates a new item below cursor
+		if ev.Modifiers() == 1 {
+			newItem(c.i)
+			c.Down()
+		}
+
+		// Enter edit mode at cursor
+		*m = true
+	case tcell.KeyBackspace2:
+	case tcell.KeyUp:
+		c.Up()
+	case tcell.KeyDown:
+		c.Down()
+	case tcell.KeyLeft:
+	case tcell.KeyRight:
+	case tcell.KeyRune:
 	case tcell.KeyTab:
 	case tcell.KeyBacktab:
 	}
--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gdamore/tcell/v2"
-	"strconv"
 	"strings"
 )
 
 func main() {
 	s := startup()
-
-	var bstr, lks, mks string
-	X, Y := s.Size()
 
 	root := parseTxt("text/example")
 
@@ -21,6 +16,9 @@ func main() {
 		i: root.Tail[0],
 	}
 
+	// Open listty with edit mode off
+	m := EditMode(false)
+
 	for {
 		s.Clear()
 		emitStr(s, 4, 3+c.y, tcell.StyleDefault, ">")
@@ -28,11 +26,6 @@ func main() {
 		for index, line := range lines {
 			emitStr(s, 5, 3+index, black, line)
 		}
-		drawBox(s, X-42-1, Y-7, X-2, Y-2, white, ' ')
-		emitStr(s, X-42, Y-6, white, "Press ESC to exit")
-		emitStr(s, X-42, Y-5, white, fmt.Sprintf("Buttons: %s", bstr))
-		emitStr(s, X-42, Y-4, white, fmt.Sprintf("Keys: %s", lks))
-		emitStr(s, X-42, Y-3, white, fmt.Sprintf("Mods: %s", mks))
 		emitStr(s, 5, 0, black, "File: "+root.Head)
 		emitStr(s, 5, 1, black, "Item path: "+strings.Join(c.i.Path(), " > "))
 		s.Show()
@@ -41,9 +34,7 @@ func main() {
 		case *tcell.EventResize:
 			s.Sync()
 		case *tcell.EventKey:
-			lks = ev.Name()
-			mks = strconv.Itoa(int(ev.Modifiers()))
-			handleEventKey(ev, s, &c)
+			handleEventKey(ev, s, &c, &m)
 		}
 	}
 }
